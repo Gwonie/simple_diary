d0b3931083ba92bb33d84c7232eaa53ce38d00ee
MinJiwon <jwjwmin111@gmail.com>
2022-09-08 14:50:24 +0900

    데이터 수정 기능 추가

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,6 +1,6 @@
 import DiaryItem from "./DiaryItem";
 
-const DiaryList = ({ diaryList, onDelete }) => {
+const DiaryList = ({ diaryList, onRemove, onEdit }) => {
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
@@ -8,7 +8,12 @@ const DiaryList = ({ diaryList, onDelete }) => {
       <div>
         {diaryList.map((it) => (
           // 객체 구조분해하여 props로 넘겨주기
-          <DiaryItem key={it.id} {...it} onDelete={onDelete} />
+          <DiaryItem
+            key={it.id}
+            {...it}
+            onRemove={onRemove}
+            onEdit={onEdit}
+          />
           // key로 인덱스 값을 사용하면 배열 수정, 삭제 시에 문제 발생 가능
         ))}
       </div>
