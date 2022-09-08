d282beb43f51ff5f8148da53cf30825dd1b66fd9
MinJiwon <jwjwmin111@gmail.com>
2022-09-08 13:39:17 +0900

    데이터 삭제 기능 추가

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,6 +1,6 @@
 import DiaryItem from "./DiaryItem";
 
-const DiaryList = ({ diaryList }) => {
+const DiaryList = ({ diaryList, onDelete }) => {
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
@@ -8,7 +8,7 @@ const DiaryList = ({ diaryList }) => {
       <div>
         {diaryList.map((it) => (
           // 객체 구조분해하여 props로 넘겨주기
-          <DiaryItem key={it.id} {...it} />
+          <DiaryItem key={it.id} {...it} onDelete={onDelete} />
           // key로 인덱스 값을 사용하면 배열 수정, 삭제 시에 문제 발생 가능
         ))}
       </div>
